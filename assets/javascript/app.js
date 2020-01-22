527453013d01252e9b14aaf4bef994d7cccbdc29
Patrick Sime <psime86@gmail.com>
2020-01-21 23:36:42 -0600

    got ratings to appear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,17 +14,18 @@ function gifDisplay(){
     })
 
     .then(function(response){
+        console.log(response);
         // Remove GIFs from GIF div
         $(".gifDiv").empty();
 
         // For loop and variables for sizing, rating, still/animate, and adding HTLM
-        for (let i = 0; i < response.length; i++){
+        for (var i = 0; i < response.data.length; i++){
             var rating = response.data[i].rating;
-            var ratingDiv = $("<p>").html("Ratins: " + rating);
+            var ratingDiv = $("<p>").html("Rating: " + rating);
             var still = response.data[i].images.fixed_height_still.url;
             var animate = response.data[i].images.fixed_height.url;
-            var newGifDiv = $('<div calss="newGifDiv>');
-            var gif = $('<img calss="gifImage');
+            var newGifDiv = $("<div calss='newGifDiv'>");
+            var gif = $('<img calss="gifImage">');
 
             // Setting GIFs to still
             gif.attr("scr", still);
@@ -41,14 +42,41 @@ function gifDisplay(){
 }
 
         // On click to animate GIFs
+        $(".gifDiv").on("click", ".gifImage", function(){
+            var state = $(this).attr("data-state");
 
-
-
+            // If else animate/still
+            if (state === "still"){
+                $(this).attr("src", $(this).data("animate"));
+                $(this).attr("data-state", "animate");
+            }
+            else{
+                $(this).attr("src", $(this).data("still"));
+                $(this).attr("data-state", "still");
+            }
+        })
 
         // Add new button function and append to current buttons
-
-
+        function addButtons(){
+            $(".buttons").empty();
+            for (var i = 0; i < simpsons.length; i++){
+                var addButton = $("<button class='newButton'>");
+                addButton.addClass("simpsons");
+                addButton.attr("data-name", simpsons[i]);
+                addButton.html(simpsons[i]);
+                $(".buttons").append(addButton);
+            }
+        }
 
         // Creating new button when a new GIF is searched
-
+        $(".search").on("click", function(event){
+            event.preventDefault();
+            var gifs = $(".search-bar").val().trim();
+            simpsons.push(gifs);
+            $(".search-bar").val("")
+            addButtons();
+        })
+    
+        $(document).on("click", ".simpsons", gifDisplay);
+        addButtons();
 })
