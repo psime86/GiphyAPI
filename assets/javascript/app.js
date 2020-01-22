b0afef8382b4d1791068e2333b3e54f0b1f5d55b
Patrick Sime <psime86@gmail.com>
2020-01-21 21:35:57 -0600

    pseudocode, HTML, and vars done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,15 +14,41 @@ function gifDisplay(){
     })
 
     .then(function(response){
+        // Remove GIFs from GIF div
+        $(".gifDiv").empty();
 
+        // For loop and variables for sizing, rating, still/animate, and adding HTLM
+        for (let i = 0; i < response.length; i++){
+            var rating = response.data[i].rating;
+            var ratingDiv = $("<p>").html("Ratins: " + rating);
+            var still = response.data[i].images.fixed_height_still.url;
+            var animate = response.data[i].images.fixed_height.url;
+            var newGifDiv = $('<div calss="newGifDiv>');
+            var gif = $('<img calss="gifImage');
 
+            // Setting GIFs to still
+            gif.attr("scr", still);
+            gif.attr("data-still", still);
+            gif.attr("data-animate", animate);
+            gif.attr("data-state", "still");
 
-        
+            // Attaching ratings after their GIF
+            newGifDiv.append(ratingDiv);
+            newGifDiv.prepend(gif);
+            $(".gifDiv").prepend(newGifDiv);
+        }
     })
 }
 
+        // On click to animate GIFs
 
 
 
+
+        // Add new button function and append to current buttons
+
+
+
+        // Creating new button when a new GIF is searched
 
 })
